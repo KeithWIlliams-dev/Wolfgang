b657b3dbb2336b6aac9e8f0821bfe45ef7ba5b1f
KeithWilliams-dev <kwilliams.dev@gmail.com>
2021-03-13 13:28:42 -0600

    Added .gitIgnore, Added Wolfgang Visual Studio 2019 project with CMAKE

---
+++ WolfgangDAW/WolfgangEditor/Main.cpp
@@ -0,0 +1,16 @@
+﻿// WolfgangEditor.cpp : Defines the entry point for the application.
+//
+
+#include "Main.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc > 1)
+	{
+		return 1;
+	}
+	cout << "Hello CMake." << endl;
+	return 0;
+}
